c200ccd170b29dcfc4820514d7cc8589c85fb425
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-11-16 15:04:27 +0100

    :lipstick: update page resume for antigaspi

--- src/componants/Resume.jsx
+++ src/componants/Resume.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Table, Tag, Modal, List } from "antd";
-import {AiOutlineEye} from "react-icons/ai";
+import { AiOutlineEye } from "react-icons/ai";
 import "../styles/styles.css";
+import { ProduitAntigaspi } from "../../SVG/ProduitAntigaspi";
 
 function Resume() {
   const [tableData, setTableData] = useState([]);
   const [hasOrders, setHasOrders] = useState(true);
-  const [selectedOrder, setSelectedOrder] = useState(null); 
-  const [isModalVisible, setIsModalVisible] = useState(false); 
-  const [orderProducts, setOrderProducts] = useState([]); 
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [orderProducts, setOrderProducts] = useState([]);
 
   const baseUrl = import.meta.env.VITE_REACT_API_URL;
 
@@ -20,7 +21,7 @@ function Resume() {
   const fetchOrders = async () => {
     try {
       const response = await axios.get(`${baseUrl}/allOrders`);
-      console.log(response.data)
+      console.log(response.data);
       if (response.data.orders && response.data.orders.length === 0) {
         setHasOrders(false);
       } else {
@@ -47,70 +48,68 @@ function Resume() {
       firstname: order.firstname_client,
       lastname: order.lastname_client,
       paymentMethod: order.paymentMethod,
-      paid:order.paid,
-      createdAt: order.createdAt
+      paid: order.paid,
+      createdAt: order.createdAt,
+      cartString: order.cartString,
     }));
   };
 
   function formatDate(dateString) {
     const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-  
+
     return `${day}/${month}/${year}`;
   }
 
   const columns = [
     {
-      title: 'N° de commande',
-      dataIndex: 'numero_commande',
-      key: 'numero_commande',
+      title: "N° de commande",
+      dataIndex: "numero_commande",
+      key: "numero_commande",
     },
     {
-      title: 'Client',
-      dataIndex: 'client',
-      key: 'client',
+      title: "Client",
+      dataIndex: "client",
+      key: "client",
     },
     {
-      title: 'Prix total',
-      dataIndex: 'prix_total',
-      key: 'prix_total',
+      title: "Prix total",
+      dataIndex: "prix_total",
+      key: "prix_total",
     },
-    
-    {
-      title: 'Passée le',
-      dataIndex: 'createdAt',
-      key: 'createdAt',
-      render: text => formatDate(text),
 
+    {
+      title: "Passée le",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => formatDate(text),
     },
     {
-      title: 'Statut',
-      dataIndex: 'status',
-      key: 'status',
+      title: "Statut",
+      dataIndex: "status",
+      key: "status",
       filters: [
-        { text: 'En attente', value: 'en attente' },
-        { text: 'Livrées', value: 'livree' },
-        { text: 'Prêtes', value: 'prete' },
-        { text: 'Annulées', value: 'annulee' },
-
-
+        { text: "En attente", value: "en attente" },
+        { text: "Livrées", value: "livree" },
+        { text: "Prêtes", value: "prete" },
+        { text: "Annulées", value: "annulee" },
       ],
       onFilter: (value, record) => record.status.indexOf(value) === 0,
       render: (status) => {
-        let color = 'default';
-        if (status === 'en attente') color = 'orange';
+        let color = "default";
+        if (status === "en attente") color = "orange";
         // else if (status === 'preparation') color = 'blue';
-        else if (status === 'prete') color = 'green';
-        else if (status === 'livree') color = 'yellow';
-        else if (status === 'annulee') color = 'red';
+        else if (status === "prete") color = "green";
+        else if (status === "livree") color = "yellow";
+        else if (status === "annulee") color = "red";
         return <Tag color={color}>{status}</Tag>;
       },
     },
     {
-      key: 'action',
-      title: 'Actions',
+      key: "action",
+      title: "Actions",
       render: (record) => (
         <>
           <AiOutlineEye onClick={() => viewOrder(record)} />
@@ -123,8 +122,10 @@ function Resume() {
     // console.log('Selected order for viewing:', record);
     try {
       // Si votre backend supporte la récupération d'une commande spécifique par son ID
-      const productsResponse = await axios.get(`${baseUrl}/getOrderProducts/${record.key}`);
-      console.log('Order products:', productsResponse.data);
+      const productsResponse = await axios.get(
+        `${baseUrl}/getOrderProducts/${record.key}`
+      );
+      console.log("Order products:", productsResponse.data);
       setOrderProducts(productsResponse.data); // Mettre à jour l'état avec les produits de la commande
       setSelectedOrder({ ...record, products: productsResponse.data }); // Mettre à jour l'état avec les détails de la commande et les produits
       setIsModalVisible(true);
@@ -132,7 +133,6 @@ function Resume() {
       console.error("Error fetching order details:", error);
     }
   };
-  
 
   const handleCancel = () => {
     setIsModalVisible(false);
@@ -140,60 +140,91 @@ function Resume() {
 
   const getStatusTagColor = (status) => {
     switch (status) {
-      case 'en attente':
-        return 'orange';
-      case 'preparation':
-        return 'blue';
-      case 'prete':
-        return 'green';
-      case 'livree':
-        return 'yellow';
-      case 'annulee':
-        return 'red';
+      case "en attente":
+        return "orange";
+      case "preparation":
+        return "blue";
+      case "prete":
+        return "green";
+      case "livree":
+        return "yellow";
+      case "annulee":
+        return "red";
       default:
-        return 'default';
+        return "default";
     }
   };
 
   const renderOrderDetailsModal = () => {
     // Utilisez un formatage approprié pour la date et les autres données si nécessaire
-    const dateFormat = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    const dateFormat = { year: "numeric", month: "2-digit", day: "2-digit" };
+    let productsInCart = [];
 
-    
+    if (selectedOrder && selectedOrder.cartString) {
+      try {
+        // Si cartString est une chaîne JSON, convertissez-la en objet JavaScript
+        productsInCart = JSON.parse(selectedOrder.cartString);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la conversion de cartString en JSON",
+          error
+        );
+        // Gérer l'erreur ou utiliser une valeur par défaut
+      }
+    }
     return (
       <Modal
-        title={`Détails de la commande ${selectedOrder ? selectedOrder.numero_commande : ''}`}
+        title={`Détails de la commande ${
+          selectedOrder ? selectedOrder.numero_commande : ""
+        }`}
         open={isModalVisible}
         onCancel={handleCancel}
         footer={null}
-        className='modalDetails'
+        className="modalDetails"
       >
         {selectedOrder && (
           <div>
-            <p>Nom du client: {selectedOrder.lastname} {selectedOrder.firstname}</p>
+            <p>
+              Nom du client: {selectedOrder.lastname} {selectedOrder.firstname}
+            </p>
             <p>N° de commande: {selectedOrder.numero_commande}</p>
-            <p>Statut:  
-            <Tag color={getStatusTagColor(selectedOrder.status)} className='tagOrder'>
-              {selectedOrder.status}
-            </Tag>
+            <p>
+              Statut:
+              <Tag
+                color={getStatusTagColor(selectedOrder.status)}
+                className="tagOrder"
+              >
+                {selectedOrder.status}
+              </Tag>
             </p>
             {/* <p>Date de la commande: {new Date(selectedOrder.date).toLocaleDateString(undefined, dateFormat)}</p> */}
             <p>Passée le : {formatDate(selectedOrder.createdAt)}</p>
             <p>Date de la commande : {selectedOrder.date}</p>
             {/* <p>Heure de la commande: {selectedOrder.heure || 'Non spécifiée'}</p> */}
             <p>Prix total: {selectedOrder.prix_total}€</p>
-            <p>Méthode de paiement: {selectedOrder.paymentMethod === 'onsite' ? 'Sur place' : "En ligne"}</p>
-            <p>Commande payée: {selectedOrder.paid ? 'Oui' : 'Non'}</p>
+            <p>
+              Méthode de paiement:{" "}
+              {selectedOrder.paymentMethod === "onsite"
+                ? "Sur place"
+                : "En ligne"}
+            </p>
+            <p>Commande payée: {selectedOrder.paid ? "Oui" : "Non"}</p>
+            
           </div>
         )}
         <List
           itemLayout="horizontal"
           dataSource={orderProducts}
-          renderItem={item => (
+          renderItem={(item) => (
             <List.Item>
               <List.Item.Meta
-                title={item.libelle}
-                description={`Quantité : ${item.quantity}`}
+                title={ 
+                <>
+                  {item.antigaspi && <span className="pastilleAntigapiResume"><ProduitAntigaspi /></span>  }
+                  {item.libelle}
+                </>
+                }
+                // description={`X ${item.quantity}`}
               />
             </List.Item>
           )}
@@ -202,19 +233,19 @@ function Resume() {
     );
   };
 
-
-  return (
-    hasOrders ? (
-      <>
-        <div className="resume-page">
-          <Table dataSource={tableData} columns={columns} pagination={{ position: ["bottomCenter"], pageSize: 6 }} />
-        </div>
-        {renderOrderDetailsModal()}
-
-      </>
-    ) : (
-      <p>Pas de commandes</p>
-    )
+  return hasOrders ? (
+    <>
+      <div className="resume-page">
+        <Table
+          dataSource={tableData}
+          columns={columns}
+          pagination={{ position: ["bottomCenter"], pageSize: 6 }}
+        />
+      </div>
+      {renderOrderDetailsModal()}
+    </>
+  ) : (
+    <p>Pas de commandes</p>
   );
 }
 
